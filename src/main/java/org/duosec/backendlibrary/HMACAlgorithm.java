990ac04e38246147d36ceb8d43c03b330941539e
Anjali <anjali184a@gmail.com>
2022-11-20 18:47:37 +0530

    Added HMAC Algorithm options enum

---
+++ src/main/java/org/duosec/backendlibrary/HMACAlgorithm.java
@@ -0,0 +1,16 @@
+package org.duosec.backendlibrary;
+
+public enum HMACAlgorithm {
+    // HMAC hashing algorithm options
+    SHA1("HmacSHA1"), SHA256("HmacSHA256"), SHA512("HmacSHA512");
+
+    private final String name;
+
+    HMACAlgorithm(String name) {
+        this.name = name;
+    }
+
+    public String getHMACName() {
+        return name;
+    }
+}
