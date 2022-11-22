423c4d3bff1d28ccb5d7dd53874c5a6f8268958d
Anjali <anjali184a@gmail.com>
2022-11-22 12:50:54 +0530

    OTP generation functionality

--- src/main/java/org/duosec/backendlibrary/TOTP.java
+++ src/main/java/org/duosec/backendlibrary/TOTP.java
@@ -1,5 +1,12 @@
 package org.duosec.backendlibrary;
 
+import org.apache.commons.codec.binary.Base32;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.util.Arrays;
 
@@ -18,6 +25,66 @@ public class TOTP {
         this.algorithm = builder.algorithm;
         this.secret = builder.secret;
         this.period = builder.period;
+    }
+
+    private String generate(final long counter) throws IllegalStateException {
+        if (counter < 0)
+            throw new IllegalArgumentException("Counter must be greater than or equal to 0");
+
+        byte[] secretBytes = decodeBase32(secret);
+        byte[] counterBytes = longToBytes(counter);
+
+        byte[] hash;
+
+        try {
+            hash = generateHash(secretBytes, counterBytes);
+        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
+            throw new IllegalStateException();
+        }
+
+        return getCodeFromHash(hash);
+    }
+
+    private byte[] decodeBase32(final byte[] value) {
+        Base32 codec = new Base32();
+        return codec.decode(value);
+    }
+
+    private byte[] longToBytes(final long value) {
+        return ByteBuffer.allocate(Long.BYTES).putLong(value).array();
+    }
+
+    private byte[] generateHash(final byte[] secret, final byte[] data) throws InvalidKeyException, NoSuchAlgorithmException {
+        // Create a secret key with correct SHA algorithm
+        SecretKeySpec signKey = new SecretKeySpec(secret, "RAW");
+        // Mac is 'message authentication code' algorithm (RFC 2104)
+        Mac mac = Mac.getInstance(algorithm.getHMACName());
+        mac.init(signKey);
+        // Hash data with generated sign key
+        return mac.doFinal(data);
+    }
+
+    private String getCodeFromHash(final byte[] hash) {
+        int mask = ~(~0 << 4);
+
+        byte lastByte = hash[hash.length - 1];
+        int offset = lastByte & mask;
+
+        // Get 4 bytes from hash from offset to offset + 3
+        byte[] truncatedHashInBytes = {hash[offset], hash[offset + 1], hash[offset + 2], hash[offset + 3]};
+
+        // Wrap in ByteBuffer to convert bytes to long
+        ByteBuffer byteBuffer = ByteBuffer.wrap(truncatedHashInBytes);
+        long truncatedHash = byteBuffer.getInt();
+
+        // Mask most significant bit
+        truncatedHash &= 0x7FFFFFFF;
+
+        // Modulo (%) truncatedHash by 10^passwordLength
+        truncatedHash %= Math.pow(10, passwordLength);
+
+        // Left pad with 0s for an n-digit code
+        return String.format("%0" + passwordLength + "d", truncatedHash);
     }
 
     @Override
